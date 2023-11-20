44768c735533c15740c4e01ea4d3326a89b29889
Javier <javidx@gmail.com>
2023-11-20 17:41:59 +0200

    Created categories and entries scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :entries
+  resources :categories
   devise_for :users
+
+  resources :categories do
+    resources :entries
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
