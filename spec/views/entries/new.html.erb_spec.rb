4b2ec9ec5382fd8c607ddad0d31d970b8d58b3b4
Javier <javidx@gmail.com>
2023-11-22 19:30:32 +0200

    Added tests for entry show and new views

--- spec/views/entries/new.html.erb_spec.rb
+++ spec/views/entries/new.html.erb_spec.rb
@@ -2,20 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "entries/new", type: :view do
   before(:each) do
-    assign(:entry, Entry.new(
-      name: "MyString",
-      user: nil
-    ))
+    assign(:entry, Entry.new)
+    assign(:categories, [Category.new(id: 1, name: "Category 1", icon: "icon1")]) # Adjust as needed
   end
 
-  it "renders new entry form" do
+  it "renders the new entry form" do
     render
 
-    assert_select "form[action=?][method=?]", entries_path, "post" do
+    expect(rendered).to have_selector('h1', text: 'New transaction')
 
-      assert_select "input[name=?]", "entry[name]"
+    expect(rendered).to render_template(partial: '_form')
 
-      assert_select "input[name=?]", "entry[user_id]"
+    expect(rendered).to have_link("Back to transactions", href: entries_path)
+
+    assert_select 'form[action=?][method=?]', entries_path, 'post' do
+      assert_select 'input[name=?]', 'entry[name]'
+      assert_select 'input[name=?]', 'entry[amount]'
+      
+      assert_select 'input[type=?]', 'checkbox' do
+        assert_select '[name=?]', 'entry[category_ids][]'
+      end
+
+      assert_select 'input[type=?]', 'submit'
     end
   end
 end
